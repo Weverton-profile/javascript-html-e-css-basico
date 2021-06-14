d26f51e7525e4bb2141849e99fef95abb69215d2
Weverton Alves <wevertonalves.profile@gmail.com>
2021-06-14 15:48:02 -0300

    modificações (gerador de cpf)

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import GeraCpf from './modules/GeraCpf';
-
-import './assets/css/style.css';
-
-(function(){
-    const gera = new GeraCpf();
-    const cpfGerado = document.querySelector('.cpf-gerado');
-    cpfGerado.innerHTML = gera.geraNovoCpf();
-})();
